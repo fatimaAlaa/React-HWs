bd3abba1820632f6b02374118c2eec2385a9c787
Fatima Alaa <fatimaalgburi@gmail.com>
2023-07-21 22:09:57 +0300

    React Task

---
+++ firstapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  console.log("Hello from main page");
+  return (
+    <div className='parent'>
+      This is Fatima Alaa from the main page
+    </div>
+  );
+}
+
+export default App;
---
+++ firstapp/src/child.js
@@ -0,0 +1,12 @@
+import './child.css';
+
+function Child() {
+  console.log("Hello from component page");
+    return (
+        <div className='child'>
+            This is Fatima Alaa from the child page
+        </div>
+    );
+}
+
+export default Child;
